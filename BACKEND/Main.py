e9c87e405c022554dece41ee4142005f424d19c0
Beytullah Cakir <betocakir52@gmail.com>
2024-12-23 15:22:59 +0300

    updated

--- BACKEND/Main.py
+++ BACKEND/Main.py
@@ -3,41 +3,46 @@ from flask import Flask, send_from_directory, request, jsonify
 from flask_cors import CORS
 from pyngrok import ngrok
 
-# Statik dosya yolunu ayarla
+#Reactın build alındığı dosya
 static_folder_path = os.path.abspath("../FRONTEND/myapp/build")
 app = Flask(__name__, static_folder=static_folder_path, static_url_path="/")
 CORS(app)
 
+#Ledin durumunu kontrol ediyor
+led_status ={
+    "pc":"turn_off",
+    "tv":"turn_off",
+    "light1":"turn_off",
+    "light2":"turn_off",
+    "light3":"turn_off",
+    "light4":"turn_off"
+}
 
-led_status = "kapa"
-
+#Ana sayfa
 @app.route("/")
-def serve():
-    
+def index():
     return send_from_directory(app.static_folder, "index.html")
 
+
+#Cihazların durumunu kontrol ettiğimiz metod
 @app.route("/<device>/<state>", methods=["POST", "GET"])
-def turn_on(device,state):
+def device_control(device,state):
     global led_status
-    led_status = "yak"
+    if state=="turn_on":
+        led_status = "yak"
+    elif state=="turn_off":
+        led_status="kapa"
     print(device ,":" ,state)
     return jsonify({"status": device, "command": state})
 
-@app.route("/light/turn_off", methods=["POST", "GET"])
-def turn_off():
-    global led_status
-    led_status = "kapa"
-    print("Komut: LED SÖNDÜR")
-    return jsonify({"status": "LED ", "command": "kapa"})
 
-@app.route('/status', methods=['GET'])
-def get_status():
-    return jsonify({"command": led_status})
+#Led durumunu sürekli kontrol eder
+@app.route('/<device>/status', methods=['GET'])
+def get_status(device):
+    return jsonify({"command":device})
+    #return jsonify({"command": led_status})
 
 if __name__ == '__main__':
-    
     public_url = ngrok.connect(5000)
-    print(f"Ngrok Public URL: {public_url}")
-
-    
+    #print(f"Ngrok Public URL: {public_url}")
     app.run(host='0.0.0.0', port=5000)
