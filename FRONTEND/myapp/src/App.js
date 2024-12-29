f0175e041ac0b46c21cca0824344858cd85ff83e
Mualla Şahin <muallashn11@gmail.com>
2024-12-29 15:23:31 +0300

    finals

--- FRONTEND/myapp/src/App.js
+++ FRONTEND/myapp/src/App.js
@@ -1,7 +1,6 @@
 import React, { useState } from "react";
 import { Header } from "./components/Header";
 import { SceneViewer } from "./components/SceneViewer";
-import { OpenAIChat } from "./OpenAIChat";
 import "bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
@@ -26,7 +25,6 @@ function App() {
         <section id="devices" className="py-5">
           <SceneViewer theme={theme} />
         </section>
-        <OpenAIChat />
       </main>
     </div>
   );
