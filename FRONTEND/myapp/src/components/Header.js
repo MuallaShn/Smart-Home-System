8689d977d5f110a5250968f7f0ff5049a50f9e9d
Esra Çimen <eesra8352@gmail.com>
2025-01-02 20:41:08 +0300

    update Header.js

--- FRONTEND/myapp/src/components/Header.js
+++ FRONTEND/myapp/src/components/Header.js
@@ -2,10 +2,13 @@ import React from "react";
 import { Navbar, Button } from "react-bootstrap"; // React-Bootstrap'tan Navbar ve Button bileşenlerini içe aktarıyoruz.
 import { Sun, Moon } from "react-bootstrap-icons"; // React-Bootstrap-Icons'tan Güneş ve Ay ikonlarını içe aktarıyoruz.
 
+// Header bileşeni tanımlanıyor. Tema durumu ve temayı değiştirme işlevi props olarak alınıyor.
 export function Header({ theme, toggleTheme }) {
 
   return (
-    <Navbar bg={theme === "dark" ? "dark" : "light"} variant={theme === "dark" ? "dark" : "light"} className="shadow-sm">
+    // Navbar bileşeni, temaya göre arka plan ve varyantını dinamik olarak belirliyor.
+    // Arka plan rengi tema "dark" ise koyu, aksi halde açık olacak.
+    <Navbar bg={theme === "dark" ? "dark" : "light"} variant={theme === "dark" ? "dark" : "light"} className="shadow-sm"> 
       <div className="container">
         <Navbar.Brand href="#home" className="fw-bold fs-4">SmartHome</Navbar.Brand>
 
